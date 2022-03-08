29fa2ff798318619aba5cea2e925c9d9d3896a40
Baba Naidu <naidubaba02@gmail.com>
2022-03-08 14:41:25 +0000

    updated hello world and added goodbye.java

---
+++ goobye.java
@@ -0,0 +1,5 @@
+class Goodbyejava{  
+    public static void main(String args[]){  
+     System.out.println("Goodbye java");  
+    }  
+}  
--- helloworld.java
+++ helloworld.java
@@ -1,3 +1,5 @@
+//this pgm will print sample msg
+
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
